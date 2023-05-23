b21653f93cb184e4d0d39dc5b454b8b63d4aeb21
KyahWill <knownbutnotloved@gmail.com>
2023-05-23 16:21:40 +0800

    updated everything

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<header>
+    <div>
+  <div class="flex flex-row">
+      <h1>World Travel Expo </h1>
+      <div>
+        <p>
+          test
+        <p>
+      </div>
+      <a href="/Register">Register </a>
+    </div>
+  </div>
+</header>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+  
+</style>
+
