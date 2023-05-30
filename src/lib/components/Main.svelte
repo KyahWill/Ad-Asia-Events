fb63584c98a529a4eaf01635ac44c4a24e911889
KyahWill <knownbutnotloved@gmail.com>
2023-05-30 17:45:57 +0800

    did a lot of change in the animation

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -6,31 +6,30 @@
     import {elasticInOut} from 'svelte/easing';
 
     const bg_urls = [
-      '/images/background.jpg',
-      '/images/bg_2.jpg',
+      '/images/background_north_luzon.jpg',
+      '/images/background_central_visayas.jpg',
+      '/images/background_southern_mindanao.jpg',
+      '/images/background_western_mindanao.jpg',
+      '/images/background_western_visayas.jpg',
+      '/images/background_metro_manila.jpg',
     ]
-    const logo_urls = [
-      '/images/main_logo.png',
-      '/images/main_logo.png'
-    ]
+
     const change_bg = async() =>{
       let counter = 0;
       while(true) {
 
          main.style.backgroundImage = 'url(\''+bg_urls[counter] + '\')'
-        header_logo.src = logo_urls[counter]
         await new Promise(r => setTimeout(r, 3000));
          counter = (counter + 1) % bg_urls.length
 
      } 
     }
     let main: HTMLElement
-    let header_logo: HTMLImageElement
     onMount(()=> {
      change_bg() 
     })
 </script>
-<main bind:this={main} class="bg-main flex items-center justify-center pt-32">
+<main bind:this={main} class="bg-main flex items-end justify-center pt-32">
     <div class="pb-20 flex flex-col items-center">
     <div style="position:absolute; top:150px; left:20px;"> 
       <img src="/images/balloon_1.png" alt="baloons"  style="position:relative;top:0px;left:0px;" class="baloon h-20" />
@@ -47,7 +46,6 @@
     <div style="position:absolute; top:300px; left:200px;" > 
       <img src="/images/balloon_2.png" alt="baloons" style="position:relative;top:0px;left:0px;" class="baloon h-28" />
     </div>
-      <img bind:this={header_logo} alt="World Travel Expo" class="mt-2 h-96">
             <!-- <div class="flex gap-5 w-96 items-center justify-center my-7"> -->
             <!--     <span class="flex w-5 h-5 bg-white rounded-full cursor-pointer"></span> -->
             <!--     <span class="flex w-5 h-5 bg-gray-200 opacity-60 rounded-full cursor-pointer"></span> -->
@@ -57,7 +55,7 @@
             <!--     <span class="flex w-5 h-5 bg-gray-200 opacity-60 rounded-full cursor-pointer"></span> -->
             <!--     <span class="flex w-5 h-5 bg-gray-200 opacity-60 rounded-full cursor-pointer"></span> -->
             <!-- </div> -->
-            <button class="bg-white text-primary text-2xl px-4 rounded-3xl py-1">
+            <button class="bg-white text-primary mb-16 text-2xl px-4 rounded-3xl py-1">
                 Join the World Travel Expo 2023
             </button>
       <img src="/images/city.png" alt="cities images" style="position:absolute;bottom:0;height:300px" />
@@ -67,7 +65,8 @@
 <style>
     .bg-main {
         background-image: url('/images/background.jpg');
-        background-size: cover;
+        background-size: contain;
+      background-repeat: no-repeat;
         width: 100%;
         height: 100vh;
     }
