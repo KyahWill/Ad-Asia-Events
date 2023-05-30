c48c17bbf7cfaf8859a9ca68a755bbc21ffed94e
KyahWill <knownbutnotloved@gmail.com>
2023-05-30 17:30:22 +0800

    did a lot of change in the animation

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -32,11 +32,21 @@
 </script>
 <main bind:this={main} class="bg-main flex items-center justify-center pt-32">
     <div class="pb-20 flex flex-col items-center">
-      <img src="/images/balloon_1.png" alt="baloons" style="position:absolute; top:150px; left:20px;" class="h-20" />
-      <img src="/images/balloon_2.png" alt="baloons" style="position:absolute; top:290px; right:30px;" class="h-20" />
-      <img src="/images/balloon_3.png" alt="baloons" style="position:absolute; top:500px; left:80px;" class="h-20" />
-      <img src="/images/balloon_1.png" alt="baloons" style="position:absolute; top:400px; right:200px;" class="h-28" />
-      <img src="/images/balloon_2.png" alt="baloons" style="position:absolute; top:300px; left:200px;" class="h-28" />
+    <div style="position:absolute; top:150px; left:20px;"> 
+      <img src="/images/balloon_1.png" alt="baloons"  style="position:relative;top:0px;left:0px;" class="baloon h-20" />
+    </div>
+    <div style="position:absolute; top:290px; right:30px;" > 
+      <img src="/images/balloon_2.png" alt="baloons" style="position:relative;top:0px;left:0px;" class="baloon h-20" />
+    </div>
+    <div style="position:absolute; top:500px; left:80px;" > 
+      <img src="/images/balloon_3.png" alt="baloons" style="position:relative;top:0px;left:0px;" class="baloon h-20" />
+    </div>
+    <div style="position:absolute; top:400px; right:200px;" > 
+      <img src="/images/balloon_1.png" alt="baloons" style="position:relative;top:0px;left:0px;" class="baloon h-28" />
+    </div>
+    <div style="position:absolute; top:300px; left:200px;" > 
+      <img src="/images/balloon_2.png" alt="baloons" style="position:relative;top:0px;left:0px;" class="baloon h-28" />
+    </div>
       <img bind:this={header_logo} alt="World Travel Expo" class="mt-2 h-96">
             <!-- <div class="flex gap-5 w-96 items-center justify-center my-7"> -->
             <!--     <span class="flex w-5 h-5 bg-white rounded-full cursor-pointer"></span> -->
@@ -50,7 +60,7 @@
             <button class="bg-white text-primary text-2xl px-4 rounded-3xl py-1">
                 Join the World Travel Expo 2023
             </button>
-      <img src="/images/city.png" style="position:absolute;bottom:0" />
+      <img src="/images/city.png" alt="cities images" style="position:absolute;bottom:0;height:300px" />
     </div>
 </main>
 
@@ -61,4 +71,17 @@
         width: 100%;
         height: 100vh;
     }
+.baloon {
+  animation: MoveUpDown 6s linear infinite;
+}
+
+@keyframes MoveUpDown {
+  0%, 100% {
+    top: 0px;
+  }
+  50% {
+    top: 100px;
+  }
+}
+
 </style>
